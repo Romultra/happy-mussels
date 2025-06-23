0d26f33a8ae3057e111eff548c85f2ce57040c0a
Romultra <romultra@gmail.com>
2025-06-23 03:26:29 +0200

    Refactor MQTT setup and remove unused reconnect logic

--- src/main-mqtt.cpp
+++ src/main-mqtt.cpp
@@ -1,17 +1,13 @@
 #include <config.h>
 #include <secrets.h>
 #include <utils.h>
+#include <network.h>
 #include <PID_v1.h>
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <time.h>
-
-const char* mqttHost = "sonarr.yperion.dev";
-const uint16_t mqttPort = 8883;
-
-WiFiClientSecure secureClient;
-PubSubClient mqtt(secureClient);
+#include <mqtt.h>
 
 const char* ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 3600;
@@ -23,51 +19,23 @@ PID tempPID(&tempInput, &tempOutput, &tempSetpoint,20,5,1, REVERSE);
 double tempValue = 25; // Simulate initial temperature value
 long lastMsg = 0;
 
-void onMqttMessage(char* topic, byte* payload, unsigned int len);
-
 void setup() {
   Serial.begin(115200);
   while(!Serial);
-
   connectToWiFi();
 
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 
-  secureClient.setCACert(caCert);
+  initMQTT();
 
-  mqtt.setServer(mqttHost, mqttPort);
-  mqtt.setCallback(onMqttMessage);
+  initPumpPWM(); // Initialize pump PWM pins
+  setPumpSpeed(255); // Set initial pump speed to 128 (50% duty cycle)
   
   tempInput = tempValue; // Set initial temperature input
   tempSetpoint = 18.0; // Set desired 
   
   tempPID.SetMode(AUTOMATIC);
   tempPID.SetOutputLimits(0, 100); // Set output limits for the PID controller
-}
-
-void reconnect() {  
-  // Loop until we're reconnected
-  while (!mqtt.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (mqtt.connect("ESP32Client")) {
-      Serial.println("connected");
-      // Subscribe
-      mqtt.subscribe("musselfarm/settemp");
-
-      mqtt.subscribe("musselfarm/pid/p");
-      mqtt.subscribe("musselfarm/pid/i");
-      mqtt.subscribe("musselfarm/pid/d");
-
-      mqtt.subscribe("musselfarm/lamp");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(mqtt.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
 }
 
 void loop() {
@@ -95,54 +63,5 @@ void loop() {
     char tempString[8];
     dtostrf(tempValue, 1, 2, tempString);
     mqtt.publish("musselfarm/temperature", tempString);
-
-  }
-}
-
-void onMqttMessage(char* topic, byte* payload, unsigned int len) {
-  String msg;
-  for (unsigned int i = 0; i < len; i++) msg += (char)payload[i];
-  Serial.println("Received [" + String(topic) + "]: " + msg);
-
-  // Parse JSON / act on commands here…
-
-  if (String(topic) == "esp32/settemp") {
-    // Convert payload to double and set as new setpoint
-    tempSetpoint = msg.toDouble();
-    Serial.print("New Setpoint: ");
-    Serial.println(tempSetpoint);
-  }
-
-  if (String(topic) == "musselfarm/pid/p") {
-    double p = msg.toDouble();
-    tempPID.SetTunings(p, tempPID.GetKi(), tempPID.GetKd());
-    Serial.print("PID P set to: ");
-    Serial.println(p);
-  }
-
-  if (String(topic) == "musselfarm/pid/i") {
-    double i = msg.toDouble();
-    tempPID.SetTunings(tempPID.GetKp(), i, tempPID.GetKd());
-    Serial.print("PID I set to: ");
-    Serial.println(i);
-  }
-
-  if (String(topic) == "musselfarm/pid/d") {
-    double d = msg.toDouble();
-    tempPID.SetTunings(tempPID.GetKp(), tempPID.GetKi(), d);
-    Serial.print("PID D set to: ");
-    Serial.println(d);
-  }
-
-  if (String(topic) == "musselfarm/lamp") {
-    if (msg == "ON") {
-      Serial.println("Lamp ON");
-      // Code to turn on the lamp
-    } else if (msg == "OFF") {
-      Serial.println("Lamp OFF");
-      // Code to turn off the lamp
-    } else {
-      Serial.println("Unknown command for lamp: " + msg);
-    }
   }
 }
